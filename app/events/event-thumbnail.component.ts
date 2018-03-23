d09396b92b11caf15ea388e36d137b5a23d4c89f
scottmacaz <scottmacaz@yahoo.com>
2018-03-23 08:16:52 -0700

    Reverted changes.  Build is broken

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
 
-@Component({selector: 'event-thumbnail', template: `
+@Component({
+  selector: 'event-thumbnail',
+  template: `
   <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>date: {{event.date}}</div>
@@ -10,25 +12,24 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
             <span *ngSwitchDefault>Normal Start</span>
           </div>
           <div>price: \${{event.price}}</div>
-        <!-- <div *ngIf="event?.location"> -->
-        <div [hidden]="!event?.location">
+        <div>
+
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            
         </div>
-        <div *ngIf="event?.onlineUrl">
-          Online Url: {{event?.onlineUrl}}
-        </div>
+        
     </div>
-  `, styles: [`
+  `,
             .green {color: #003300 !important; }
             .bold { font-weight: bold;}
-            .thumbnail {min-height: 210px;}
-            .pad-left {margin-left: 10px;}
-            .well div {color: #bbb;}`]})
+  styles: [`.pad-left {margin-left: 10px;}
+            .well div {color: #bbb;}`]
+})
 
 export class EventThumbnailComponent {
 
-  @Input()event : any;
+  @Input() event: any;
   getStartTimeClass() {
     if (this.event && this.event.time === '8:00 am') {
       return 'green bold';
@@ -36,5 +37,6 @@ export class EventThumbnailComponent {
     return '';
     
   }
-
+  
+ 
 }
