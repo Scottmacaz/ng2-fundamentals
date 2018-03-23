d5d7872bda4bfee354208bce63955764583bec4d
unknown <scottmacaz@yahoo.com>
2018-03-23 05:46:10 -0700

    updates that broke the UI

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,18 +4,24 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
   <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>date: {{event.date}}</div>
-        <div>time: {{event.time}}</div>
-        <div>price: \${{event.price}}</div>
+        <div [ngClass] = "getStartTimeClass()" [ngSwitch]="event?.time">time: {{event.time}}
+            <span *ngSwitchCase="'8:00 am'">Early Start</span>
+            <span *ngSwitchCase="'10:00 am'">Late Start</span>
+            <span *ngSwitchDefault>Normal Start</span>
+          </div>
+          <div>price: \${{event.price}}</div>
         <!-- <div *ngIf="event?.location"> -->
         <div [hidden]="!event?.location">
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
         <div *ngIf="event?.onlineUrl">
           Online Url: {{event?.onlineUrl}}
         </div>
     </div>
   `, styles: [`
+            .green {color: #003300 !important; }
+            .bold { font-weight: bold;}
             .thumbnail {min-height: 210px;}
             .pad-left {margin-left: 10px;}
             .well div {color: #bbb;}`]})
@@ -23,5 +29,12 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 export class EventThumbnailComponent {
 
   @Input()event : any;
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am') {
+      return 'green bold';
+    }
+    return '';
+    
+  }
 
 }
